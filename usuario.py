7b9e0b92a0e70bb1e6c95d65b5df15347cc671d5
Paula Gómez Lucas <130698085+paula-gomezlucas@users.noreply.github.com>
2025-12-16 00:36:55 +0100

    Added original files from private repo

---
+++ usuario.py
@@ -0,0 +1,62 @@
+from conbbdd import conbbdd
+import hashlib
+
+
+class usuario:
+    def __init__(self, nombre, passw, email, esGestor):
+        self.nombre = nombre
+        self.passw = passw
+        self.email = email
+        self.esGestor = bool(esGestor)
+
+    def acceder(self):
+        query = "SELECT EMAIL FROM USUARIO WHERE EMAIL = '{email}' AND ALTA = 1;".format(email=self.email)
+        mydb = conbbdd.conectar('195.235.211.197', 'psi_grupo2', 'psi_grupo2', '1234')
+        datos = conbbdd.queryBBDD(mydb, query)
+
+        if datos:
+            comprobarpassw = "SELECT HASHPASS FROM USUARIO WHERE EMAIL = '{email}';".format(email=self.email)
+            acceso = conbbdd.queryBBDD(mydb, comprobarpassw)
+
+            if hashlib.md5(self.passw.encode()).hexdigest() == acceso[0][0]:
+                comprobarautorizacion = "SELECT ROL, NOMBRE FROM USUARIO WHERE EMAIL = '{email}';".format(email=self.email)
+                lista = conbbdd.queryBBDD(mydb, comprobarautorizacion)
+                self.esGestor = bool(lista[0][0])
+                self.nombre = lista[0][1]
+                conbbdd.desconectar(mydb)
+                return True
+
+            else:
+                conbbdd.desconectar(mydb)
+                return False
+
+        else:
+            conbbdd.desconectar(mydb)
+            return False
+
+    def registroBBDD(self):
+        query = "SELECT EMAIL FROM USUARIO WHERE EMAIL = '{email}';".format(email=self.email)
+        mydb = conbbdd.conectar('195.235.211.197', 'psi_grupo2', 'psi_grupo2', '1234')
+        datos = conbbdd.queryBBDD(mydb, query)
+
+        if datos:
+            conbbdd.desconectar(mydb)
+            return False
+        else:
+            hashpass = hashlib.md5(self.passw.encode()).hexdigest()
+            registroUsuario = '''INSERT INTO USUARIO (EMAIL, NOMBRE, HASHPASS, ROL, ALTA)\
+                              VALUES ('{email}', '{nombre}', 
+                              '{passw}', 0, 1);'''.format(email=self.email, nombre=self.nombre, passw=hashpass)
+            conbbdd.queryBBDD(mydb, registroUsuario)
+
+    def accesoBBDD(self, fila):
+        query = "SELECT EMAIL, NOMBRE FROM USUARIO WHERE EMAIL = '{fila}';".format(fila=fila)
+        mydb = conbbdd.conectar('195.235.211.197', 'psi_grupo2', 'psi_grupo2', '1234')
+        respuesta = conbbdd.queryBBDD(mydb, query)
+        conbbdd.desconectar(mydb)
+        return respuesta
+
+    def altaBaja(self, email, alta):
+        query = "UPDATE USUARIO SET ALTA = {alta} WHERE EMAIL = '{email}';".format(email=email, alta=alta)
+        mydb = conbbdd.conectar('195.235.211.197', 'psi_grupo2', 'psi_grupo2', '1234')
+        conbbdd.queryBBDD(mydb, query)
